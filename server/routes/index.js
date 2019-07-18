271439a02b73862be0d24effc9c784755795b876
Macbook Air <mhtrmkholis@gmail.com>
2019-07-18 20:45:26 +0700

    1st commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,11 @@
 const routes = require("express").Router()
-const User = require("./user.js")
-const Image = require("./image.js")
+// const User = require("./user.js")
+// const Image = require("./image.js")
+const wikipedia = require('./wikipedia');
 
-routes.use("/", User)
-routes.use("/image", Image)
+// routes.use("/", User)
+// routes.use("/image", Image)
+routes.use('/wikipedia', wikipedia)
+
 
 module.exports = routes
