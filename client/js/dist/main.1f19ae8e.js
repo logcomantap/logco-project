909b224d72922790334accfc1526017f1e6c8396
viuty <you@example.com>
2019-07-18 20:42:25 +0700

    revisi

--- client/js/dist/main.1f19ae8e.js
+++ client/js/dist/main.1f19ae8e.js
@@ -9094,7 +9094,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46435" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44841" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
