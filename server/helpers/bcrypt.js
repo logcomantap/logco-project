df715125b42ad81a4e0cb1829c9d15e4d2595291
imam madani <iafgon@yahoo.com>
2019-07-18 19:44:49 +0700

    user register, login done

---
+++ server/helpers/bcrypt.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcryptjs');
+module.exports = {
+	generateHash(password) {
+		const salt = bcrypt.genSaltSync(10);
+		return bcrypt.hashSync(password, salt);
+	},
+	compareHash(password, hash) {
+		return bcrypt.compareSync(password, hash);
+	}
+};
